65e8d0c67e454ff00bfe87bba6aef56d3448bc54
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-06-15 11:36:11 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from './Component/Navbar';
+
 function App() {
   return (
     <>
-      <h1>Home</h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/Component/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <header className="text-white body-font bg-gradient-to-b from-[#6506EF]  to-[#A872F5] shadow ">
+        <div className="container mx-auto flex justify-center items-center p-3 ">
+          <p className="flex title-font font-medium ">
+            <span className="ml-3 text-xl">Profile</span>
+          </p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
