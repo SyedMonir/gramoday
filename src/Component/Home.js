63505d89368e486d4ba43a007715eddd5908527d
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-06-15 12:43:21 +0600

    fixed issue and updated the style of home header

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import { FaUserCircle } from 'react-icons/fa';
+import { AiOutlineUserAdd } from 'react-icons/ai';
+import { IoMdCall } from 'react-icons/io';
 
 const Home = () => {
   const {
@@ -15,22 +18,32 @@ const Home = () => {
   if (isLoading) return 'Loading...';
 
   if (error) {
-    console.log('An error has occurred: ' + error.message);
+    console.log(error.message);
   }
 
-  console.log(user);
+  //   console.log(user);
   return (
     <section className="w-auto sm:w-1/2 mx-auto px-4">
-      <div className="flex gap-6">
-        <div>profile</div>
+      <div className="flex gap-6 my-4">
+        <div className="mt-4">
+          <FaUserCircle color="#616161" size={82} />
+        </div>
         <div>
-          <h1>{user?.name}</h1>
-          <h5>{user?.loclevel3Name + ', ' + user?.loclevel2Name}</h5>
-          <h6>Speak, {user?.language === 'en' ? 'English' : user?.language}</h6>
+          <h1 className="font-bold text-xl">{user?.name}</h1>
+          <h5 className="my-2">
+            {user?.loclevel3Name + ', ' + user?.loclevel2Name}
+          </h5>
+          <h6 className="text-xs italic mb-4">
+            Speak, {user?.language === 'en' ? 'English' : user?.language}
+          </h6>
 
-          <div>
-            <button>Connect</button>
-            <button>Contact</button>
+          <div className="flex gap-2">
+            <button className="btn flex items-center bg-[#01A39D] text-white rounded-3xl px-2 py-1 text-sm">
+              <AiOutlineUserAdd className="mr-1" /> Connect
+            </button>
+            <button className="btn flex items-center text-[#01A39D] border border-[#01A39D] rounded-3xl px-2 py-1 text-sm">
+              <IoMdCall className="mr-1" /> Contact
+            </button>
           </div>
         </div>
       </div>
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
       <header className="text-white body-font bg-gradient-to-b from-[#6506EF]  to-[#A872F5] shadow ">
         <div className="container mx-auto flex justify-center items-center p-3 ">
           <p className="flex title-font font-medium ">
-            <span className="ml-3 text-xl">Profile</span>
+            <span className="ml-3 text-xl font-bold">Profile</span>
           </p>
         </div>
       </header>
